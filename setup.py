d7c9da8968a209236dfec06da0d059c9b86dad74
William Graf <7930703+willgraf@users.noreply.github.com>
2020-06-22 15:34:45 -0700

    no encoding for python3.

--- setup.py
+++ setup.py
@@ -28,8 +28,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 # read the contents of your README file
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md')) as f:
     long_description = f.read()
 
 
