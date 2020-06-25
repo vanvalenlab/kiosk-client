c69f95132707903f5c1f1e24a20bb40221f4be80
willgraf <7930703+willgraf@users.noreply.github.com>
2020-06-25 13:37:33 -0700

    Upload each job synchronously before starting the next job. (#62)
    * `BatchProcessingJobManager` will wait for a job to upload before starting the next job.
    
    * `twisted[tls]` is installed by `treq`. No need to include both in the requirements.
    
    * Overriding headers for `upload_file` is not required.
    
    * No need to stagger the `start_delay` if we are uploading each file synchronously.

--- kiosk_client/manager.py
+++ kiosk_client/manager.py
@@ -319,10 +319,12 @@ class BatchProcessingJobManager(JobManager):
     def run(self, filepath):
         self.logger.info('Benchmarking all image/zip files in `%s`', filepath)
 
-        for i, f in enumerate(iter_image_files(filepath)):
+        for f in iter_image_files(filepath):
             job = self.make_job(f)
             self.all_jobs.append(job)
+            uploaded_path = yield job.upload_file()
+            job.filepath = os.path.relpath(uploaded_path, self.upload_prefix)
             # stagger the delay seconds
-            job.start(delay=self.start_delay * i, upload=True)
+            job.start(delay=self.start_delay, upload=False)
 
         yield self.check_job_status()
